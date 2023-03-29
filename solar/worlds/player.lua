f5c4a4ca8256d1324fc71a2c48e1ef5e02e782b4
solar-projects <serialexperimentalchannel@gmail.com>
2023-03-29 19:47:25 -0300

    looking at + interactions update.

--- solar/worlds/player.lua
+++ solar/worlds/player.lua
@@ -24,6 +24,17 @@ function Solar_DrawPlayer(engine, world_mode, player)
   if player.draw_using == consts.SOLAR_DRAW_USING_COLOR then
     love.graphics.setColor(utils.Solar_TranslateColor(player.color))
     love.graphics.rectangle("fill", player.rel_position.x, player.rel_position.y, player.size.x, player.size.y)
+    -- TODO: remove on the future versions, this is just for debugging LOL.
+    local dot_looking_direction_position = {
+      [consts.SOLAR_PLAYER_LOOKING_UP]    = {player.rel_position.x+math.floor(player.size.x/2), player.rel_position.y},
+      [consts.SOLAR_PLAYER_LOOKING_DOWN]  = {player.rel_position.x+math.floor(player.size.x/2), player.rel_position.y+player.size.y},
+      [consts.SOLAR_PLAYER_LOOKING_LEFT]  = {player.rel_position.x, player.rel_position.y+math.floor(player.size.y/2)},
+      [consts.SOLAR_PLAYER_LOOKING_RIGHT] = {player.rel_position.x+player.size.x-1, player.rel_position.y+math.floor(player.size.y/2)}
+    }
+    local current_looking_position=dot_looking_direction_position[player.looking_at]
+    love.graphics.setColor(utils.Solar_TranslateColor(utils.Solar_NewColor(255, 255, 255)))
+    love.graphics.rectangle("fill", current_looking_position[1], current_looking_position[2], 1, 1)
+    --
   end
 end
 module.Solar_DrawPlayer = Solar_DrawPlayer

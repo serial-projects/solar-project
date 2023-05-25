089135f90af516b79d2d9ba816593611a624b37b
serial-projects <serialexperimentalchannel@gmail.com>
2023-05-25 12:06:58 -0300

    new label + code cleaning.

--- sol/wmode.lua
+++ sol/wmode.lua
@@ -22,9 +22,12 @@ function Sol_InitWorldModeGUI(engine, world_mode)
   local begun_initializing_gui=os.clock() ; dmsg("Sol_InitWorldModeGUI() was called!")
   local normal_font=storage.Sol_LoadFontFromStorage(engine.storage, "terminal", 14)
   world_mode.main_display=ui.Sol_NewDisplay({size=engine.viewport_size})
+  --> some "macros" for the UI
+  local function _GenerateDebugUIGenericLabel() return ui.Sol_NewLabel({font=normal_font, color=consts.colors.white, has_background=true, background_color=consts.colors.black}) end
+
   --> display_debug_ui: frame
     world_mode.display_debug_ui_frame=ui.Sol_NewFrame()
-      world_mode.display_debug_ui_frame_version_label=ui.Sol_NewLabel({font=normal_font})
+      world_mode.display_debug_ui_frame_version_label=_GenerateDebugUIGenericLabel()
         world_mode.display_debug_ui_frame_version_label.text=string.format(
           storage.Sol_GetText(engine.storage,"WMODE_DISPLAY_DEBUG_UI_FRAME_VERSION_LABEL_TEXT"),
           defaults.SOL_VERSION,
@@ -33,10 +36,12 @@ function Sol_InitWorldModeGUI(engine, world_mode)
         )
         world_mode.display_debug_ui_frame_version_label.position=smath.Sol_NewVector(0, 0)
         world_mode.display_debug_ui_frame_version_label.force_absolute=false
-        world_mode.display_debug_ui_frame_version_label.color=consts.colors.white
-        world_mode.display_debug_ui_frame_version_label.has_background=true
-        world_mode.display_debug_ui_frame_version_label.background_color=consts.colors.black
         ui.Sol_InsertElementInFrame(world_mode.display_debug_ui_frame, world_mode.display_debug_ui_frame_version_label)
+      world_mode.display_debug_ui_frame_memoryusage_label=_GenerateDebugUIGenericLabel()
+        world_mode.display_debug_ui_frame_memoryusage_label.non_formatted_text=storage.Sol_GetText(engine.storage, "WMODE_DISPLAY_DEBUG_UI_FRAME_MEMORYUSAGE_LABEL_TEXT")
+        world_mode.display_debug_ui_frame_memoryusage_label.position=smath.Sol_NewVector(100, 0)
+        world_mode.display_debug_ui_frame_memoryusage_label.force_absolute=false
+        ui.Sol_InsertElementInFrame(world_mode.display_debug_ui_frame, world_mode.display_debug_ui_frame_memoryusage_label)
     ui.Sol_InsertElement(world_mode.main_display, world_mode.display_debug_ui_frame)
   --> display_inv_ui: frame
   dmsg("Sol_InitWorldModeGUI() finished building GAME UI at %fs", os.clock()-begun_initializing_gui)
@@ -56,7 +61,13 @@ function Sol_InitWorldMode(engine, world_mode)
 end ; module.Sol_InitWorldMode=Sol_InitWorldMode
 
 --[[ Tick Related Functions ]]
+function Sol_TickWorldModeUI(engine, world_mode)
+  --> update: world_mode.display_debug_ui_frame_memoryusage_label
+  local amount_used_memory=tostring(collectgarbage("count") / 1000):sub(1, 4)
+  world_mode.display_debug_ui_frame_memoryusage_label.text=string.format(world_mode.display_debug_ui_frame_memoryusage_label.non_formatted_text, amount_used_memory)
+end
 function Sol_TickWorldMode(engine, world_mode)
+  Sol_TickWorldModeUI(engine, world_mode)
   ui.Sol_TickDisplay(world_mode.main_display)
   if world_mode.current_world then
     local current_world=world_mode.worlds[world_mode.current_world]

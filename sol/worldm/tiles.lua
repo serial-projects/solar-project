b3faf368bf79107c904139bee5e6cb92534c1561
serial-projects <serialexperimentalchannel@gmail.com>
2023-05-28 14:19:08 -0300

    textures is now working!

--- sol/worldm/tiles.lua
+++ sol/worldm/tiles.lua
@@ -1,5 +1,6 @@
 local smath=require("sol.smath")
 local defaults=require("sol.defaults")
+local storage=require("sol.storage")
 local module={}
 
 --
@@ -16,24 +17,40 @@ function Sol_NewTile(tile)
     color = smath.Sol_NewColor4(tile["color"]),
     textures = tile["textures"] or {},
     texture_index = 1,
-    texture_timing = 0,
+    texture_timing = tile["texture_timing"] or 1,
+    texture_nextupdate=0,
     current_chunk = {}
   }
 end ; module.Sol_NewTile=Sol_NewTile
 
 --[[ Draw Related Functions ]]
 function Sol_DrawTile(engine, world_mode, world, tile)
+  local rxpos, rypos = smath.Sol_GetTileRelativePosition(world_mode.player.rel_position, world_mode.player.rectangle.position, tile.rectangle.position)
+  local width, height= smath.Sol_UnpackVectorXY(tile.rectangle.size)
   if tile.draw_method == defaults.SOL_DRAW_USING.COLOR then
+    --[[ Draw using the color ]]--
     love.graphics.setColor(smath.Sol_TranslateColor(tile.color))
-    local rxpos, rypos = smath.Sol_GetTileRelativePosition(world_mode.player.rel_position, world_mode.player.rectangle.position, tile.rectangle.position)
-    local width, height= smath.Sol_UnpackVectorXY(tile.rectangle.size)
     love.graphics.rectangle("fill", rxpos, rypos, width, height)
   else
-    mwarn("not implemented drawing method, falling back to color.")
-    tile.draw_method = defaults.SOL_DRAW_USING.COLOR
-    -- NOTE: there is not actually problem to call this since we changed
-    -- the color drawing method so there is no problem with eternal recursion.
-    Sol_DrawTile(engine, world_mode, world, tile)
+    --[[ Draw using the texture ]]--
+    local current_texture
+    if type(tile.textures) == "string" then
+      current_texture=storage.Sol_LoadImageFromStorage(engine.storage, tile.textures)
+      love.graphics.setColor(1, 1, 1, 1)
+      love.graphics.draw(current_texture, rxpos, rypos)
+    elseif type(tile.textures) == "table" then
+      -- TODO: when the tile is using textures as a table, it must be a sprite table.
+      local o_image, quad = storage.Sol_LoadSpriteFromStorage(engine.storage, tile.textures[tile.texture_index])
+      love.graphics.setColor(1, 1, 1, 1)
+      love.graphics.draw(o_image, quad, rxpos, rypos)
+      if os.clock()>=tile.texture_nextupdate then
+        tile.texture_index=(tile.texture_index+1> #tile.textures) and 1 or tile.texture_index+1
+        tile.texture_nextupdate=os.clock()+tile.texture_timing
+      end
+    else
+      tile.draw_method = defaults.SOL_DRAW_USING.COLOR
+      return
+    end
   end
 end ; module.Sol_DrawTile=Sol_DrawTile
 

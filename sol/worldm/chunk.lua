4deafad412111c9d313702b4b4ae8dda1a84d423
serial-projects <serialexperimentalchannel@gmail.com>
2023-05-28 14:49:52 -0300

    Sol_MapChunksInWorld is on the chunk module.

--- sol/worldm/chunk.lua
+++ sol/worldm/chunk.lua
@@ -8,6 +8,28 @@ function Sol_GetPlayerCurrentChunk(world_mode, world)
   return player_current_chunk_x, player_current_chunk_y
 end ; module.Sol_GetPlayerCurrentChunk=Sol_GetPlayerCurrentChunk
 
+function Sol_MapChunksInWorld(world)
+  -- TODO: on the future make this code threaded to prevent the game lagging when creating a lot of tiles.
+  local begun_mapping_chunks_at=os.clock()
+  world.chunks={}
+  for tile_index, tile in ipairs(world.tiles) do
+    if tile.type == "tile" then
+      local tile_belongs_chunk_inx=math.floor(tile.rectangle.position.x/(world.bg_tile_size.x*defaults.SOL_WORLD_CHUNK_WIDTH))
+      local tile_belongs_chunk_iny=math.floor(tile.rectangle.position.y/(world.bg_tile_size.y*defaults.SOL_WORLD_CHUNK_HEIGHT))
+      local chunk_reference=tostring(tile_belongs_chunk_inx)..'.'..tostring(tile_belongs_chunk_iny)
+      if world.chunks[chunk_reference] then
+        table.insert(world.chunks[chunk_reference], tile_index)
+        table.insert(tile.current_chunk, chunk_reference)
+      else
+        world.chunks[chunk_reference]={}
+        table.insert(world.chunks[chunk_reference], tile_index)
+        table.insert(tile.current_chunk, chunk_reference)
+      end
+    end
+  end
+  dmsg("Sol_MapChunksInWorld() took %fs", os.clock()-begun_mapping_chunks_at)
+end ; module.Sol_MapChunksInWorld=Sol_MapChunksInWorld
+
 --[[ Functions used in Tick ]]
 function Sol_GetChunksReferencedTiles(world, indexx, indexy, range)
   local adquired_references = {}

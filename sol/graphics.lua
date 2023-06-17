418084651c6ebb64be24b76cbecafc3287c07814
serial-projects <serialexperimentalchannel@gmail.com>
2023-06-17 16:08:42 -0300

    less squares on the background!

--- sol/graphics.lua
+++ sol/graphics.lua
@@ -11,13 +11,14 @@ function module.Sol_GeneratePixelatedPatternUsingAlpha(canva, properties)
   local pastc = love.graphics.getCanvas()
   love.graphics.setCanvas(canva)
   --
-  love.graphics.clear(0.2, 0.2, 0.2, 0.4)
+  love.graphics.clear(10/255, 10/255, 10/255, 0.7)
   --
   local NBLOCKS_Y = math.floor(CH / PXHEIGHT)
   local NBLOCKS_X = math.floor(CW / PXWIDTH)
   for yindex = 0, NBLOCKS_Y do
     for xindex = 0, NBLOCKS_X do
       local amount_filled = (NBLOCKS_Y / 100) * ( (NBLOCKS_Y / (yindex/ 2) ) * 100 )
+      amount_filled = math.floor(amount_filled / 0.5)
       love.graphics.setColor(200/255, 200/255, 200/255, math.random(0, 255 - amount_filled) / 255)
       love.graphics.rectangle("fill", xindex * PXWIDTH, yindex *PXHEIGHT, PXWIDTH, PXHEIGHT)
     end

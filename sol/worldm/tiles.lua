4bd1b25e813a1f28fc262cc714f069329d0e9386
serial-projects <serialexperimentalchannel@gmail.com>
2023-06-26 17:45:26 -0300

    interaction control.

--- sol/worldm/tiles.lua
+++ sol/worldm/tiles.lua
@@ -19,6 +19,7 @@ function module.Sol_NewTile(tile)
     draw = drawrec.Sol_NewDrawRecipe(tile["draw"]),
     --[[ actions ]]--
     when_touched                = tile["when_touched"] or 0,
+    enable_interaction          = tile["enable_interaction"] or false,
     when_interacted             = tile["when_interacted"] or 0,
   }
 end

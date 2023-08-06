cd2cdbef190127a3108f1d2aef6f12e3dc41066e
serial-projects <serialexperimentalchannel@gmail.com>
2023-08-06 14:57:42 -0300

    better project tree with better naming.

--- sol/engine.lua
+++
@@ -1,5 +0,0 @@
--- engine.lua: load all the extra functions and return the REAL engine.
-local xlog      =require("sol.xlog")
-local xstring   =require("sol.xstring")
-local xtable    =require("sol.xtable")
-return require("sol._engine")

cad8bacaf447612fe1aecaa66a5e602ace549679
serial-projects <serialexperimentalchannel@gmail.com>
2023-09-04 09:38:51 -0300

    interact is now split to it own module.

---
+++ Solar/Modes/World/Interact.lua
@@ -0,0 +1,49 @@
+local module = {}
+
+local SWM_Chunk   = require("Solar.Modes.World.Chunk")
+local SSE_Script  = require("Solar.Services.Script")
+
+local SV_Defaults = require("Solar.Values.Defaults")
+local SV_Consts   = require("Solar.Values.Consts")
+
+local SM_Vector   = require("Solar.Math.Vector")
+local SM_Rectangle= require("Solar.Math.Rectangle")
+
+function module.Sol_DoInteractionInWorld(engine, world_mode, world, tile, interaction_recipe)
+  interaction_recipe["when_finish"] = function(ir)
+    tile.busy = false
+  end
+  tile.busy = true
+  SSE_Script.Sol_LoadScriptInWorld(engine, world_mode, world, world.scripts, interaction_recipe)
+end
+
+function module.Sol_AttemptInteractionInWorld(engine, world_mode, world)
+  -- TODO: on the future, make a RAY that hit some tile to check possible interactions.
+  -- USING this method with env. PRECISE_WALK disabled MAY result in problems and less
+  -- precise interactions. Load all the tiles from the current chunk & load looking table:
+  local current_chunk_tiles=SWM_Chunk.Sol_GetChunksOrdered(engine, world_mode, world, false)
+  local SOL_PLAYER_INTERACTION_RANGE=SV_Defaults.SOL_PLAYER_INTERACTION_RANGE
+  local test_position={
+    [SV_Consts.player_directions.UP]     =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x, world_mode.player.rectangle.position.y-SOL_PLAYER_INTERACTION_RANGE),
+    [SV_Consts.player_directions.DOWN]   =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x, world_mode.player.rectangle.position.y+SOL_PLAYER_INTERACTION_RANGE),
+    [SV_Consts.player_directions.LEFT]   =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x-SOL_PLAYER_INTERACTION_RANGE, world_mode.player.rectangle.position.y),
+    [SV_Consts.player_directions.RIGHT]  =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x+SOL_PLAYER_INTERACTION_RANGE, world_mode.player.rectangle.position.y),
+  }
+  
+  -- build the rectangle:
+  local testing_rectangle=SM_Rectangle.Sol_NewRectangle(test_position[world_mode.player.looking_direction], world_mode.player.rectangle.size)
+  
+  -- begin reading the tiles in-search of possible interactions. ALSO, ignore '1' the player.
+  for _, tile in ipairs(current_chunk_tiles) do
+    local current_tile=world.tiles[tile.target]
+    if current_tile.enable_interaction and not current_tile.busy then
+      local has_collision=SM_Rectangle.Sol_TestRectangleCollision(testing_rectangle, current_tile.rectangle)
+      if has_collision then
+        module.Sol_DoInteractionInWorld(engine, world_mode, world, current_tile, current_tile.when_interacted)
+        break
+      end
+    end
+  end
+end
+
+return module
--- Solar/Modes/World/World.lua
+++ Solar/Modes/World/World.lua
@@ -13,6 +13,7 @@ local SWM_Load      = require("Solar.Modes.World.Load")
 local SWM_Player    = require("Solar.Modes.World.Player")
 local SWM_Tiles     = require("Solar.Modes.World.Tiles")
 local SWM_Chunk     = require("Solar.Modes.World.Chunk")
+local SWM_Interact  = require("Solar.Modes.World.Interact")
 
 local module={}
 --
@@ -68,53 +69,10 @@ end
 
 --[[ Keypress Event Related Functions ]]--
 
-function module.Sol_DoInteractionInWorld(engine, world_mode, world, tile, interaction_recipe)
-  -- NOTE: to prevent the element from triggering multiple stances of threads (aka. bouncy), we lock the file from
-  -- interacting again. When the HALT instruction is called or the script finish, then unlock it.
-  
-  -- TODO: allow some scripts to run in a mode called "sattelite mode", on this mode, the script instance is not
-  -- deleted from the script service when finished, it stays dormant until next interaction. This is very useful
-  -- for tiles that need multiple interactions, for now, THE only way to do multiple interaction stuff is by using
-  -- global variables (WHICH SHOULD ONLY BE USED FOR VERY IMPORTANT DATA AND NEED TO SAVE DATA!).
-  interaction_recipe["when_finish"], tile.busy=(function(ir) tile.busy = false end), true
-  SSE_Script.Sol_LoadScriptInWorld(engine, world_mode, world, world.scripts, interaction_recipe)
-end
-
-function module.Sol_AttemptInteractionInWorld(engine, world_mode, world)
-  -- TODO: on the future, make a RAY that hit some tile to check possible interactions.
-  -- USING this method with env. PRECISE_WALK disabled MAY result in problems and less
-  -- precise interactions.
-  -- load all the tiles from the current chunk & load looking table:
-  local current_chunk_tiles=SWM_Chunk.Sol_GetChunksOrdered(engine, world_mode, world)
-  local SOL_PLAYER_INTERACTION_RANGE=SV_Defaults.SOL_PLAYER_INTERACTION_RANGE
-  local test_position={
-    [SV_Consts.player_directions.UP]     =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x, world_mode.player.rectangle.position.y-SOL_PLAYER_INTERACTION_RANGE),
-    [SV_Consts.player_directions.DOWN]   =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x, world_mode.player.rectangle.position.y+SOL_PLAYER_INTERACTION_RANGE),
-    [SV_Consts.player_directions.LEFT]   =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x-SOL_PLAYER_INTERACTION_RANGE, world_mode.player.rectangle.position.y),
-    [SV_Consts.player_directions.RIGHT]  =SM_Vector.Sol_NewVector(world_mode.player.rectangle.position.x+SOL_PLAYER_INTERACTION_RANGE, world_mode.player.rectangle.position.y),
-  }
-  -- build the rectangle:
-  local testing_rectangle=SM_Rectangle.Sol_NewRectangle(test_position[world_mode.player.looking_direction], world_mode.player.rectangle.size)
-  -- begin reading the tiles in-search of possible interactions. ALSO, ignore '1' the player.
-  for _, tile in ipairs(current_chunk_tiles) do
-    if tile.target ~= 1 then
-      local current_tile=world.tiles[tile.target]
-      if current_tile.enable_interaction and not current_tile.busy then
-        local has_collision=SM_Rectangle.Sol_TestRectangleCollision(testing_rectangle, current_tile.rectangle)
-        -- dmsg("%s tile, %s enabled interaction.", current_tile.name, current_tile.enable_interaction and "yes" or "no")
-        if has_collision then
-          module.Sol_DoInteractionInWorld(engine, world_mode, world, current_tile, current_tile.when_interacted)
-          break
-        end
-      end
-    end
-  end
-end
-
 -- Sol_KeypressEventWorld(engine, world_mode, world, key: string)
 function module.Sol_KeypressEventWorld(engine, world_mode, world, key)
   if key == engine.wmode_keymap["interact"] then
-    module.Sol_AttemptInteractionInWorld(engine, world_mode, world)
+    SWM_Interact.Sol_AttemptInteractionInWorld(engine, world_mode, world)
   end
 end
 

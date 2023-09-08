d5f5d422cb761351a5eef532cac0b2aff166ae9c
serial-projects <serialexperimentalchannel@gmail.com>
2023-09-08 16:02:25 -0300

    impl. Solar Symbolic File .

---
+++ Solar/Files/SSF.lua
@@ -0,0 +1,10 @@
+local module = {}
+--
+function module.Sol_BuildSolarSymbolicFile(target_table, configuration)
+  local binary_buffer = {}
+end
+function module.Sol_DecodeSolarSymbolicBuffer(buffer)
+
+end
+--
+return module

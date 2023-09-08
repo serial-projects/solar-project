e8d286613c971322afd7ba6409d194f8dc0f154e
serial-projects <serialexperimentalchannel@gmail.com>
2023-09-08 10:41:38 -0300

    sysc to change the world.

--- Solar/Services/SCInWorldLevel.lua
+++ Solar/Services/SCInWorldLevel.lua
@@ -9,6 +9,7 @@ local module = {}
 -- * GAME  : this script can execute in all worlds.
 -- * WORLD : this script can only execute in isolated worlds.
 function module.Sol_ImplementInWorldSystemCalls(engine, world_mode, world, context)
+  local begun_registering_functions=os.clock()
   --[[ Player Absolute Position ]]--
   context.system_calls["SolWorld_GetPlayerPosition"]=function(_, instance)
     local xpos, ypos = SM_Vector.Sol_UnpackVectorXY(world_mode.player.rectangle.position)
@@ -29,6 +30,23 @@ function module.Sol_ImplementInWorldSystemCalls(engine, world_mode, world, conte
     table.insert(instance.stack, world_mode.player.rectangle.size.y)
   end
   --[[ World Stuff ]]--
+  --> :: functions to change the world
+  context.system_calls["SolWorld_RequestWorldLoad"]=function(_, instance)
+    local world_name = instance.registers.X
+    world_mode.load_world_request = {name = world_name, change = false}
+  end
+  context.system_calls["SolWorld_SetCurrentWorld"]=function(_, instance)
+    local world_name = instance.registers.X
+    if world_mode.worlds[world_name] then
+      world_mode.current_world = world_name
+    else
+      instance.registers.X = 0
+    end
+  end
+  context.system_calls["SolWorld_SetWorld"]=function(_, instance)
+    local world_name = instance.registers.X
+    world_mode.load_world_request = {name = world_name, change = true}
+  end
   context.system_calls["SolWorld_SetWorldSize"]=function(_, instance)
     local xpos, ypos = instance.registers.X, instance.registers.Y
     if type(xpos) == "number" and type(ypos) == "number" then
@@ -50,6 +68,7 @@ function module.Sol_ImplementInWorldSystemCalls(engine, world_mode, world, conte
       end
     end
   end
+  dmsg("finished registering world system calls in %f seconds.", os.clock() - begun_registering_functions)
 end
 
 return module

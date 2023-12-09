bc9d849b1f3ef2b76b8b2154981443d2df0eb7b1
serial-projects <serialexperimentalchannel@gmail.com>
2023-12-09 11:07:17 -0300

    remove 'l' key to crash :)

--- main.lua
+++ main.lua
@@ -1,76 +1,75 @@
 -- 2020 - 2023 Solar Engine by Pipes Studios. This project is under the MIT license.
 
-local SolarEngine = require("Solar.Engine")
-local LibraryArgparser = require("Library.Argparser.Argparser")
+local SolarEngine       = require("Solar.Engine")
+local LibraryArgparser  = require("Library.Argparser.Argparser")
 
 --[[ Globals ]]
-local SolEngineMainInstance=SolarEngine.Sol_NewEngine()
-local SolEnginePathResources="Game/"
+local SolEngineMainInstance     = SolarEngine.Sol_NewEngine()
+local SolEnginePathResources    = "Game/"
 
 --[[ love.help() ]]
 function love.help()
-  -- TODO: on the future, implement translation to this.
-  os.exit((function(lines) for _, line in ipairs(lines) do print(line) end return 0 end)(
-      {
-        ">> This is a list of commands you can use on the Sol Engine",
-        "--root/-r <path>:        defines the path for the game folder.",
-        "--log/-l <file>:         set the output for additional information.",
-        "--debug/-d:              shows additional information runtime.",
-        "--help/-h:               shows this text and quit.",
-        "--version/-v:            shows the version of the engine."
-      }
+    -- TODO: on the future, implement translation to this.
+    os.exit((function(lines) for _, line in ipairs(lines) do print(line) end return 0 end)(
+            {
+                ">> This is a list of commands you can use on the Sol Engine",
+                "--root/-r <path>:        defines the path for the game folder.",
+                "--log/-l <file>:         set the output for additional information.",
+                "--debug/-d:              shows additional information runtime.",
+                "--help/-h:               shows this text and quit.",
+                "--version/-v:            shows the version of the engine."
+            }
+        )
     )
-  )
 end
 
 --[[ love.load(args) ]]
 function love.load(args)
-  LibraryArgparser.Argparser(args, {
+    LibraryArgparser.Argparser(args, {
+        --
+        ["--debug"]={nargs=0, wrap=function()
+        
+        end}, ["-d"]="--debug",
+        ["--log"]={nargs=1, wrap=function(path)
+        
+        end}, ["-l"]="--log",
+        --
+        ["--help"] ={nargs=0, wrap=function()
+            love.help()
+        end}, ["-h"]="--help",
+        --
+        ["--root"] ={nargs=1, wrap=function(path)
+            SolEnginePathResources=path
+        end}, ["-r"]="--root",
+        --
+        ["--version"]={nargs=0, wrap=function(path)
+            local _defaults=require("sol.defaults")
+            print(string.format("Sol Engine Version: \"%s\" by Pipes Studios", _defaults.SOL_VERSION))
+        end}, ["-v"]="--version",
+        --
+        ["default"]=function(argument)
+            dmsg("unknown argument \"%s\", ignoring...", argument)
+        end,
+    })
     --
-    ["--debug"]={nargs=0, wrap=function()
-      setdebug(true)
-    end}, ["-d"]="--debug",
-    ["--log"]={nargs=1, wrap=function(path)
-      logfileset(path)
-    end}, ["-l"]="--log",
+    SolarEngine.Sol_InitEngine(SolEngineMainInstance, SolEnginePathResources)
     --
-    ["--help"] ={nargs=0, wrap=function()
-      love.help()
-    end}, ["-h"]="--help",
-    --
-    ["--root"] ={nargs=1, wrap=function(path)
-      SolEnginePathResources=path
-    end}, ["-r"]="--root",
-    --
-    ["--version"]={nargs=0, wrap=function(path)
-      local _defaults=require("sol.defaults")
-      print(string.format("Sol Engine Version: \"%s\" by Pipes Studios", _defaults.SOL_VERSION))
-    end}, ["-v"]="--version",
-    --
-    ["default"]=function(argument)
-      dmsg("unknown argument \"%s\", ignoring...", argument)
-    end,
-  })
-  --
-  SolarEngine.Sol_InitEngine(SolEngineMainInstance, SolEnginePathResources)
-  --
-  msg(">> running on lua: %s (jit? %s), love: \"%d.%d.%d\" [%s]", _VERSION, (_G["jit"] and "yes" or "no"), love.getVersion())
+    msg(">> running on lua: %s (jit? %s), love: \"%d.%d.%d\" [%s]", _VERSION, (_G["jit"] and "yes" or "no"), love.getVersion())
 end
 
 --[[ love.resize(new_width, new_height) ]]
 function love.resize(new_width, new_height)
-  SolarEngine.Sol_NewResizeEventEngine(SolEngineMainInstance, new_width, new_height)
+    SolarEngine.Sol_NewResizeEventEngine(SolEngineMainInstance, new_width, new_height)
 end
 
 --[[ love.update(dt: decimal) ]]
 function love.update(deltatime)
-  SolarEngine.Sol_TickEngine(SolEngineMainInstance)
+    SolarEngine.Sol_TickEngine(SolEngineMainInstance)
 end
 
 --[[ love.keypressed() ]]
 function love.keypressed(key)
-  if key == 'l' then qcrash(-1, "bruh") end
-  SolarEngine.Sol_KeypressEventEngine(SolEngineMainInstance, key)
+    SolarEngine.Sol_KeypressEventEngine(SolEngineMainInstance, key)
 end
 
 --[[ love.draw() ]]

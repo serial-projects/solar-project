16941e6aef0c0723f6eefcde601547f8576526e0
serial-projects <serialexperimentalchannel@gmail.com>
2023-12-09 10:05:25 -0300

    removed OneshotModule.

--- Library/OneshotModules/Load.lua
+++
@@ -1,8 +0,0 @@
--- 2020 - 2023 Solar Engine by Pipes Studios. This project is under the MIT license.
-if _G["OneshotModulesImported"] then goto skip_everything end
-local module_targets, module_prefix, begun = {"Log","String","Table","Tokenizer"}, "Solar.OneshotModules.%s", os.clock()
-for _, module in ipairs(module_targets) do
-  require(string.format(module_prefix,module))
-end ; dmsg("Oneshot Modules took %f seconds to load.", os.clock() - begun)
-_G["OneshotModulesImported"] = true
-::skip_everything::

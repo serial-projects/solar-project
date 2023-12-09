713bb23feb9f298633c41e1cb7a322047a0e06cd
serial-projects <serialexperimentalchannel@gmail.com>
2023-12-09 11:19:46 -0300

    fix spacing.

--- Solar/Math/Tile.lua
+++ Solar/Math/Tile.lua
@@ -3,7 +3,7 @@
 local module = {}
 --
 function module.Sol_GetTileRelativePosition(rel_position, abs_position, tile_position)
-  return (-abs_position.x + rel_position.x) + tile_position.x, (-abs_position.y + rel_position.y) + tile_position.y
+    return (-abs_position.x + rel_position.x) + tile_position.x, (-abs_position.y + rel_position.y) + tile_position.y
 end
 --
 return module

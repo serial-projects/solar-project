d6d2d1bf1f5850096843ff29582985d70039cfb3
serial-projects <serialexperimentalchannel@gmail.com>
2023-12-11 09:44:57 -0300

    rectangle is now a object w/ methods.

--- Solar/Math/Rectangle.lua
+++ Solar/Math/Rectangle.lua
@@ -3,41 +3,50 @@
 local module = {}
 local SM_Vector = require("Solar.Math.Vector")
 
---
 function module.Sol_NewRectangle(position, size)
-    return {
-        position  = position  or SM_Vector.Sol_NewVector(0, 0),
-        size      = size      or SM_Vector.Sol_NewVector(0, 0)
+    local proto_rectangle = {
+        position    = position or SM_Vector.Sol_NewVector(0, 0),
+        size        = size or SM_Vector.Sol_NewVector(0, 0)
     }
-end
-
-function module.Sol_CloneRectangle(rectangle)
-    return { position = SM_Vector.Sol_NewVector(rectangle.position), size = SM_Vector.Sol_NewVector(rectangle.size) }
-end
-
-function module.Sol_UnpackRectXYWH(rectangle)
-    return rectangle.position.x, rectangle.position.y, rectangle.size.x, rectangle.size.y
-end
-
-function module.Sol_TestRectangleCollision(base_rectangle, target_rectangle)
-    if target_rectangle.size.x < base_rectangle.size.x and target_rectangle.size.y < base_rectangle.size.y then
-        local ta, tb = base_rectangle.position.x + 1, base_rectangle.position.x + (base_rectangle.size.x - 1)
-        local tc, td = base_rectangle.position.y + 1, base_rectangle.position.y + (base_rectangle.size.y - 1)
-        local xa, xb = target_rectangle.position.x, target_rectangle.position.x + target_rectangle.size.x
-        local ya, yb = target_rectangle.position.y, target_rectangle.position.y + target_rectangle.size.y
-        local cx = (xa >= ta and xa <= tb) or (xb >= ta and xb <= tb)
-        local cy = (ya >= tc and ya <= td) or (yb >= tc and yb <= td)
-        if cx and cy then return true end
-    else
-        local ta, tb = target_rectangle.position.x, target_rectangle.position.x + target_rectangle.size.x
-        local tc, td = target_rectangle.position.y, target_rectangle.position.y + target_rectangle.size.y
-        local xa, xb = base_rectangle.position.x + 1, base_rectangle.position.x + (base_rectangle.size.x - 1)
-        local ya, yb = base_rectangle.position.y + 1, base_rectangle.position.y + (base_rectangle.size.y - 1)
-        local cx = (xa >= ta and xa <= tb) or (xb >= ta and xb <= tb)
-        local cy = (ya >= tc and ya <= td) or (yb >= tc and yb <= td)
-        if cx and cy then return true end
+    function proto_rectangle:clone()
+        return module.Sol_NewRectangle(
+            SM_Vector.Sol_NewVector(self.position.x, self.position.y),
+            SM_Vector.Sol_NewVector(self.size.x, self.size.y)
+        )
     end
-    return false
+    function proto_rectangle:unpackxywh()
+        return self.position.x, self.position.y, self.size.x, self.size.y
+    end
+    function proto_rectangle:collide(target_rectangle)
+        if target_rectangle.size.x < self.size.x and target_rectangle.size.y < self.size.y then
+            local ta, tb = self.position.x + 1, self.position.x + (self.size.x - 1)
+            local tc, td = self.position.y + 1, self.position.y + (self.size.y - 1)
+            local xa, xb = target_rectangle.position.x, target_rectangle.position.x + target_rectangle.size.x
+            local ya, yb = target_rectangle.position.y, target_rectangle.position.y + target_rectangle.size.y
+            local cx = (xa >= ta and xa <= tb) or (xb >= ta and xb <= tb)
+            local cy = (ya >= tc and ya <= td) or (yb >= tc and yb <= td)
+            if cx and cy then return true end
+        else
+            local ta, tb = target_rectangle.position.x, target_rectangle.position.x + target_rectangle.size.x
+            local tc, td = target_rectangle.position.y, target_rectangle.position.y + target_rectangle.size.y
+            local xa, xb = self.position.x + 1, self.position.x + (self.size.x - 1)
+            local ya, yb = self.position.y + 1, self.position.y + (self.size.y - 1)
+            local cx = (xa >= ta and xa <= tb) or (xb >= ta and xb <= tb)
+            local cy = (ya >= tc and ya <= td) or (yb >= tc and yb <= td)
+            if cx and cy then return true end
+        end
+    end
+    return proto_rectangle
 end
+
 --
+-- function module.Sol_NewRectangle(position, size)
+--     return {
+--         position  = position  or SM_Vector.Sol_NewVector(0, 0),
+--         size      = size      or SM_Vector.Sol_NewVector(0, 0)
+--     }
+-- end
+
+--
+
 return module
